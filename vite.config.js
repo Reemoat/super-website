ec2965e276a5fb380b07c9ac6c07ca96dc7ea8b5
Reemoat <bithurburn@gmail.com>
2026-07-20 21:18:18 -0700

    adding test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://Reemoat.github.io/client-toolchain-example/",
+  base: "https://github.com/Reemoat/super-website",
 });
